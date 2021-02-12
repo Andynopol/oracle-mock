674ee5fe4dfd3d2c29000209c03df91dee281fe4
Andynopol <41628030+Andynopol@users.noreply.github.com>
2021-02-12 18:14:07 +0200

    ok

--- app/src/components/Login/index.js
+++ app/src/components/Login/index.js
@@ -3,18 +3,32 @@ import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles( ( theme ) => ( {
-
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+    },
+    glass: {
+        minHeight: '80vh',
+        background: 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.3), rgba(255,255,255,0.7))',
+        borderRadius: '16px'
+    }
 } ) );
 
 export default function Login () {
     const classes = useStyles();
     return (
         <Grid container>
-            <Grid item xs={false} md={2} lg={3} />
-            <Grid item xs={12} md={8} lg={6}>
-                Login Component here
+            <Grid item xs={1} md={4} />
+            <Grid item xs={10} md={4} lg={6}>
+                <Grid container className={classes.root}>
+                    <Grid item xs={12} className={classes.glass}>
+
+                    </Grid>
+                </Grid>
             </Grid>
-            <Grid item xs={false} md={2} lg={3} />
+            <Grid item xs={1} md={4} />
         </Grid>
     );
 }
