f6af6f8f3d2dd3da371ee2c3f33b75ab267e32f3
Andynopol <41628030+Andynopol@users.noreply.github.com>
2021-02-16 15:38:05 +0200

    revamp

--- app/src/components/Login/index.js
+++ app/src/components/Login/index.js
@@ -13,9 +13,9 @@ const useStyles = makeStyles( ( theme ) => ( {
     },
     glass: {
         position: 'relative',
-        height: '550px',
+        height: '500px',
         '@media screen and (orientation: landscape)': {
-            marginTop: '50px',
+            marginTop: '20px',
         },
         background: 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.4), rgba(255,255,255,0.5))',
         borderRadius: '16px',
